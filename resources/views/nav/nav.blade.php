fe1009517d0565a0ad85bcc4ccad9a87bfd98042
andreger <andreger@gmail.com>
2022-02-01 12:21:08 -0300

    Fixed error in nav components if items array is null

--- resources/views/nav/nav.blade.php
+++ resources/views/nav/nav.blade.php
@@ -1,18 +1,20 @@
 <ul class="nav {{$classTag}}">
-    @foreach ($items as $item)
-        <li class="nav-item">
-            <a class="nav-link
-                     {{ isset($item['id']) && ($item['id'] == $selected) ? 'active' : '' }}
-                     {{ isset($item['disabled']) && $item['disabled'] == true ? 'disabled' : ''}}"
-                href="{{ $item['href'] ?? '' }}">
+    @if($items)
+        @foreach ($items as $item)
+            <li class="nav-item">
+                <a class="nav-link
+                         {{ isset($item['id']) && ($item['id'] == $selected) ? 'active' : '' }}
+                         {{ isset($item['disabled']) && $item['disabled'] == true ? 'disabled' : ''}}"
+                    href="{{ $item['href'] ?? '' }}">
 
-                @include('bs::common.fontAwesome', [
-                    'fas' => $item['fas'] ?? null,
-                    'far' => $item['far'] ?? null,
-                    'fab' => $item['fab'] ?? null,
-                ])
-                {{ $item['text'] ?? '' }}
-            </a>
-        </li>
-    @endforeach
+                    @include('bs::common.fontAwesome', [
+                        'fas' => $item['fas'] ?? null,
+                        'far' => $item['far'] ?? null,
+                        'fab' => $item['fab'] ?? null,
+                    ])
+                    {{ $item['text'] ?? '' }}
+                </a>
+            </li>
+        @endforeach
+    @endif
 </ul>
---
+++ tests/Unit/NavTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace LaravelBootstrap\Tests\Unit;
+
+use LaravelBootstrap\Tests\TestCase;
+
+class NavTest extends TestCase
+{
+    /** @test */
+    public function render_nav()
+    {
+        $view = $this->blade(
+            '<x-bs-nav />',
+        );
+
+        $view->assertSeeInOrder(['<ul', '</ul>'], false);
+    }
+
+    /** @test */
+    public function render_tab()
+    {
+        $view = $this->blade(
+            '<x-bs-tab />',
+        );
+
+        $view->assertSeeInOrder(['<ul', 'nav-tabs', '</ul>'], false);
+    }
+
+    /** @test */
+    public function render_nav_pill()
+    {
+        $view = $this->blade(
+            '<x-bs-nav-pill />',
+        );
+
+        $view->assertSeeInOrder(['<ul', 'nav-pills', '</ul>'], false);
+    }
+
+    /** @test */
+    public function render_vertical_nav()
+    {
+        $view = $this->blade(
+            '<x-bs-vertical-nav />',
+        );
+
+        $view->assertSeeInOrder(['<ul', 'flex-column', '</ul>'], false);
+    }
+}
