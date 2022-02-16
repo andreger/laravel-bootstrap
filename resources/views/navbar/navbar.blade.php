b125e62b60e9b6c263b07199af163be33a2a876b
andreger <andreger@gmail.com>
2022-02-16 17:55:15 -0300

    Removed unnecessary HTML when navbar has no items

--- resources/views/navbar/navbar.blade.php
+++ resources/views/navbar/navbar.blade.php
@@ -1,11 +1,14 @@
 <nav class="{{ $classValue }}">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">{{ $slot }}</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"  data-bs-target="#navbarItems" aria-controls="navbarItems" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarItems">
-            @include('bs::nav.nav', ['classValue' => 'navbar-nav me-auto mb-2 mb-lg-0', 'items' => $items])
-        </div>
+
+        @if ($items)
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"  data-bs-target="#navbarItems" aria-controls="navbarItems" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarItems">
+                @include('bs::nav.nav', ['classValue' => 'navbar-nav me-auto mb-2 mb-lg-0', 'items' => $items])
+            </div>
+        @endif
     </div>
 </nav>
